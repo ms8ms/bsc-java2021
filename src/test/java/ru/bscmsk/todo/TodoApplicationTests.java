a990bf7f81900990b3d38018566b83474fe7c1c4
Смирнов Михаил Юрьевич (4038340) <musmirnov@dev.vtb>
2021-11-22 17:03:38 +0500

    empty spring project

---
+++ src/test/java/ru/bscmsk/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bscmsk.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
