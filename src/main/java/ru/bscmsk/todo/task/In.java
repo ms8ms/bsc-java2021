f0d7ebac199fc33b2ce2f00dabeea9d86b125aaa
Смирнов Михаил Юрьевич (4038340) <musmirnov@dev.vtb>
2021-11-23 13:04:23 +0500

    fixing after review

--- src/main/java/ru/bscmsk/todo/task/In.java
+++ src/main/java/ru/bscmsk/todo/task/In.java
@@ -2,13 +2,13 @@ package ru.bscmsk.todo.task;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 @Slf4j
-@Service
+@Component
 public class In implements IInput {
     private final BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
 
